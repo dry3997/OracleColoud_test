de8e3a9e5d2dca54b7b91dcb21267ddff672fa4e
dry3997 <dry3997@naver.com>
2023-01-04 12:34:01 +0900

    vu init

--- demo/src/main/java/com/example/demo/emp/web/EmpController.java
+++ demo/src/main/java/com/example/demo/emp/web/EmpController.java
@@ -1,14 +1,22 @@
 package com.example.demo.emp.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.emp.mapper.EmpMapper;
+import com.example.demo.emp.service.EmpVO;
 
 @Controller
+@CrossOrigin(origins = "*") //2개서버 연동(vue,sprinig)
+//실제로 사용X 보안이 풀어진상태
 public class EmpController {
 	
 	@Autowired 
@@ -22,6 +30,12 @@ public class EmpController {
 		model.addAttribute("empList", empMapper.getEmpList(null));
 		return "empList"; 
 		//타임리프이기때문에 jsp가 아닌 html찾음
+	}
+	
+	@RequestMapping(value = "/empInfoList", method = RequestMethod.GET)
+	@ResponseBody
+	public List<EmpVO> empInfoList(Model model){
+		return empMapper.getEmpList(null);
 	}
 
 }
